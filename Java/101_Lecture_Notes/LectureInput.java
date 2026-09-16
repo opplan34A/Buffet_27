cfd9b3db724d4adf2c63527e6e183cdd26c71015
opplan34A <op_plan_alpha@outlook.com>
2026-09-16 18:00:46 +0000

    a

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,62 @@
 /*
     Lecture note example - Input!!
 */
+import java.util.Scanner;
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+    Scanner Input = new Scanner(System.in);
+    
+    double price1 = 45.25;    
+    double price2 = 35.55;
+    double price3 = 20.95;
+    double price4 = 5.59;
+
+
+    System.out.println("welcome to Ruben's eating place");
+    
+    System.out.print("Gf Pizza ");
+    System.out.print(price1);
+    System.out.println(" $");
+    
+
+    System.out.print ("Gf Pasta ");
+    System.out.print (price2); 
+    System.out.println (" $");
+    
+    System.out.print ("Gf mozzarella sticks ");
+    System.out.print (price3);
+    System.out.println (" $");
+
+    System.out.print ("RootBeer");
+    System.out.print (price4);
+    System.out.println (" $");
+
+        
+    System.out.println("How many orders of Gf pizza");
+    
+    int numinput = Input.nextInt();  
+    System.out.println("");
+  
+
+    System.out.println("How many orders of Gf pasta");
+    int numinput2 = Input.nextInt();
+    System.out.println("");
+
+    System.out.println("How many orders of Mozzarela sticks");
+    int numinput3 = Input.nextInt();
+    System.out.println(""); 
+
+    System.out.println("how many orders of Root Beer");
+    int numinput4 = Input.nextInt();
+    System.out.println(""); 
+    
+    double totalcost = (numinput*45.25)+(numinput2*35.55)+(numinput3*20.95)+(numinput4*5.59);
+    System.out.print ("total cost ");
+    System.out.print(totalcost);
+    System.out.print("$");
+
+
+
 	}
 }
